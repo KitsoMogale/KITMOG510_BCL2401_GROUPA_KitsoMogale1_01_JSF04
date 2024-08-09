ade108782435bece7e9c51f6e4685a6416957f84
Kitso Mogale <kitsomogale19@gmail.com>
2024-08-09 11:11:32 +0200

    place form in center

--- src/components/pages/login.vue
+++ src/components/pages/login.vue
@@ -1,6 +1,6 @@
 <template >
   
-  <div class="w-full max-w-sm p-6 bg-white rounded-lg shadow-md">
+  <div class="w-full max-w-sm p-6 bg-white rounded-lg shadow-md relative m-auto mt-10 ">
         <h2 class="text-2xl font-semibold text-center text-gray-800">Login</h2>
 
         <form class="mt-8 space-y-6">
@@ -16,22 +16,6 @@
                     <input id="password" name="password" type="password" autocomplete="current-password" required
                         class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                         placeholder="Password">
-                </div>
-            </div>
-
-            <div class="flex items-center justify-between mt-4">
-                <div class="flex items-center">
-                    <input id="remember_me" name="remember_me" type="checkbox"
-                        class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
-                    <label for="remember_me" class="ml-2 block text-sm text-gray-900">
-                        Remember me
-                    </label>
-                </div>
-
-                <div class="text-sm">
-                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-                        Forgot your password?
-                    </a>
                 </div>
             </div>
 
